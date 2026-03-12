8ce7f62276f6a741922ea5f900e10ab074b31bdc
n_joid <72697791+nakisara01@users.noreply.github.com>
2026-03-12 11:32:29 +0900

    [#5] style: add header css

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,30 @@
+/* eslint-disable @next/next/no-img-element */
 import "./header.css";
-import Image from "next/image"
 
 export default function Header() {
-    return <div className='header'>
-        <header className="headerObject">
-            <Image src="/daily-insight-logo.png" width={212} height={34} alt="데일리 인사이트" />
-                <a href="https://new.daily-funding.com/" target="_blank" rel="noopener noreferrer">
-                <div className="dailyFundingLink">
-                <span>데일리펀딩 바로가기</span>
-                <Image src="/dailyfunding-link.png" width={25} height={25} alt="데일리펀딩 바로가기" />
-                </div>
-                </a>
-        </header>
+  return (
+    <div className="header">
+      <header className="headerObject">
+        <img
+          className="dailyInsightLogo"
+          src="/daily-insight-logo.png"
+          alt="데일리 인사이트"
+        />
+        <a
+          href="https://new.daily-funding.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="dailyFundingLink">
+            <span>데일리펀딩 바로가기</span>
+            <img
+              className="linkSymbol"
+              src="/dailyfunding-link.png"
+              alt="데일리펀딩 바로가기"
+            />
+          </div>
+        </a>
+      </header>
     </div>
+  );
 }
