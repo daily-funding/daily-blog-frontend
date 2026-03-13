4d73e7554e327e3602640d6aef0c9ff8e589ab7a
n_joid <72697791+nakisara01@users.noreply.github.com>
2026-03-13 17:47:18 +0900

    [#12] feat: add up button

---
+++ src/components/upButton.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import Image from "next/image";
+
+export default function UpButton() {
+  const handleTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <>
+      <div className="up-btn-container">
+        <button className="up-btn" onClick={handleTop}>
+          <Image
+            src="/footer/up-button.png"
+            alt="위로 가기"
+            width={61}
+            height={61}
+          />
+        </button>
+      </div>
+
+      <style jsx>{`
+        .up-btn {
+          position: fixed;
+          right: 40px;
+          bottom: 40px;
+          z-index: 100;
+
+          width: 61px;
+          height: 61px;
+
+          border: none;
+          background: transparent;
+          cursor: pointer;
+
+          padding: 0;
+        }
+
+        .up-btn:focus {
+          outline: none;
+        }
+
+        @media (max-width: 768px) {
+          .up-btn {
+            width: 47px;
+            height: 47px;
+            right: 20px;
+            bottom: 20px;
+          }
+        }
+      `}</style>
+    </>
+  );
+}
