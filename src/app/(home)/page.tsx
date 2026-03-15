781650594121b471b3d7ab9ddc54ccdce5fb658e
n_joid <72697791+nakisara01@users.noreply.github.com>
2026-03-15 19:32:21 +0900

    [#16] feat: add category button style

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,15 @@
+import CategoryButton from "@/src/components/categoryButton";
 import TopCarousel from "@/src/components/topCarousel";
 import UpButton from "@/src/components/upButton";
+import "./home.css";
 
 export default function Home() {
   return (
-    <div>
-      <TopCarousel/>
+    <div className="home">
+      <TopCarousel />
+      <div className="postSection">
+        <CategoryButton />
+      </div>
       <UpButton />
     </div>
   );
--- src/components/categoryButton.tsx
+++ src/components/categoryButton.tsx
@@ -1,12 +1,29 @@
-import React from "react";
+"use client";
+
+import { useState } from "react";
 import { mockCategories } from "../data/mockCategories";
+import "./categoryButton.css";
 
 function getCategories() {
   return mockCategories;
 }
 
 export default function CategoryButton() {
-  const categories = getCategories();
+  const data = getCategories();
+  const categories = data.categories;
+  const [selectedIndex, setSelectedIndex] = useState(0);
 
-  return <div></div>;
+  return (
+    <div className="categorySection">
+      {categories.map((category, index) => (
+        <div
+          className={index === selectedIndex ? "active" : ""}
+          key={category.category_id}
+          onClick={() => setSelectedIndex(index)}
+        >
+          <button>{category.name}</button>
+        </div>
+      ))}
+    </div>
+  );
 }
