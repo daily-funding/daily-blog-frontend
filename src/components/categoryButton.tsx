4d6eba16f211cbe32877786e1c812ff00bbd28a1
n_joid <72697791+nakisara01@users.noreply.github.com>
2026-03-16 10:27:18 +0900

    [#16] feat: change url style about searchParams

--- src/components/categoryButton.tsx
+++ src/components/categoryButton.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import { useState } from "react";
+import { useSearchParams } from "next/navigation";
 import { mockCategories } from "../data/mockCategories";
 import "./categoryButton.css";
+import Link from "next/link";
 
 function getCategories() {
   return mockCategories;
@@ -11,19 +12,30 @@ function getCategories() {
 export default function CategoryButton() {
   const data = getCategories();
   const categories = data.categories;
-  const [selectedIndex, setSelectedIndex] = useState(0);
+  const searchParams = useSearchParams();
+  const currentCategoryId = Number(searchParams.get("category_id")) || 0;
 
   return (
     <div className="categorySection">
-      {categories.map((category, index) => (
-        <div
-          className={index === selectedIndex ? "active" : ""}
-          key={category.category_id}
-          onClick={() => setSelectedIndex(index)}
-        >
-          <button>{category.name}</button>
-        </div>
-      ))}
+      {categories.map((category) => {
+        const href =
+          category.category_id === 0
+            ? "/"
+            : `/?category_id=${category.category_id}`;
+
+        return (
+          <Link
+            href={href}
+            scroll={false}
+            key={category.category_id}
+            className={
+              category.category_id === currentCategoryId ? "active" : ""
+            }
+          >
+            <button>{category.name}</button>
+          </Link>
+        );
+      })}
     </div>
   );
 }
