a717adad52b08b1fd290ff92a89dc26a8139a613
n_joid <72697791+nakisara01@users.noreply.github.com>
2026-03-16 15:35:18 +0900

    [#17] feat: add title, more, img routing

--- src/app/(home)/components/postItem.tsx
+++ src/app/(home)/components/postItem.tsx
@@ -1,21 +1,32 @@
 /* eslint-disable @next/next/no-img-element */
-import Link from "next/link";
+"use client";
+
 import "./postList.css";
 import { Post } from "@/src/types/post";
+import { useRouter } from "next/navigation";
 
 type PostItemProps = {
-    post: Post;
+  post: Post;
 };
 
 export default function PostItem({ post }: PostItemProps) {
+  const router = useRouter();
+  const onClick = () => {
+    router.push(`/post/${post.post_id}`);
+  };
+
   return (
     <div className="article_item">
-      <img src={post.preview_image} alt="" />
+      <img src={post.preview_image} alt="" onClick={onClick} />
       <div className="post_info">
         <div className="articleCategoryBadge">{post.category_name}</div>
-        <p className="postInfoTitle">{post.title}</p>
+        <p className="postInfoTitle" onClick={onClick}>
+          {post.title}
+        </p>
         <p className="postInfoDescription">{post.description}</p>
-        <p className="postInfoMoreButton">{"MORE >"}</p>
+        <p className="postInfoMoreButton" onClick={onClick}>
+          {"MORE >"}
+        </p>
       </div>
     </div>
   );
