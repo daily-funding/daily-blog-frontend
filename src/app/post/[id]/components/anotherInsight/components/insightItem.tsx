0e77787f095aa9799db6405d4f3badb3da007e05
n_joid <72697791+nakisara01@users.noreply.github.com>
2026-03-17 17:47:58 +0900

    [#27] feat: add another insight layout

--- src/app/post/[id]/components/anotherInsight/components/insightItem.tsx
+++ src/app/post/[id]/components/anotherInsight/components/insightItem.tsx
@@ -1,10 +1,24 @@
 import { InsightItemProps } from "@/src/types/post";
 
 /* eslint-disable @next/next/no-img-element */
-export default function InsightItem(insight: InsightItemProps) {
-  <div className="insightItem">
-    <img src={insight.preview_image} alt="" />
-    <span>{insight.category_name}</span>
-    <p>{insight.title}</p>
-  </div>;
+// export default function InsightItem(insight: InsightItemProps) {
+//   <div className="insightItem">
+//     <img src={insight.preview_image} alt="" />
+//     <span>{insight.category_name}</span>
+//     <p>{insight.title}</p>
+//   </div>;
+// }
+
+export default function InsightItem() {
+  return (
+    <div className="insightItem">
+      <img src="/post/dailian-back.png" alt="" />
+      <div className="black_back">
+        <span>USER TALK</span>
+      </div>
+      <div className="info_div">
+        <p>로깅 레이어, 이슈 해결과 고객 이해를 위해 해해햏해해</p>
+      </div>
+    </div>
+  );
 }
--- src/app/post/[id]/components/anotherInsight/index.tsx
+++ src/app/post/[id]/components/anotherInsight/index.tsx
@@ -1,3 +1,4 @@
+import InsightItem from "./components/insightItem";
 import "./style.css";
 
 export default function AnotherInsight() {
@@ -5,6 +6,14 @@ export default function AnotherInsight() {
     <section className="anotherInsightSection">
       <div className="anotherInsight">
         <p className="title">또 다른 인사이트</p>
+        <div className="temptemp">
+          <InsightItem />
+          <InsightItem />
+          <InsightItem />
+          <InsightItem />
+          <InsightItem />
+          <InsightItem />
+        </div>
       </div>
     </section>
   );
