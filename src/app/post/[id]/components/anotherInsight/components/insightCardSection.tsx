e8c9269c4bda4fbecf61cf3754d8f48a6d4e2551
n_joid <72697791+nakisara01@users.noreply.github.com>
2026-03-18 15:52:06 +0900

    [#27] fix: apply insightCard Carousel style

--- src/app/post/[id]/components/anotherInsight/components/insightCardSection.tsx
+++ src/app/post/[id]/components/anotherInsight/components/insightCardSection.tsx
@@ -4,6 +4,7 @@ import { InsightItemProps } from "@/src/types/post";
 import styles from "../style.module.css";
 import InsightItem from "./insightItem";
 import { useEffect, useState } from "react";
+import useEmblaCarousel from "embla-carousel-react";
 
 export default function InsightCardSection({
   posts,
@@ -11,24 +12,43 @@ export default function InsightCardSection({
   posts: InsightItemProps[];
 }) {
   const [isMobile, setIsMobile] = useState(false);
+  const [emblaRef] = useEmblaCarousel();
 
   const checkMobile = () => {
     setIsMobile(window.innerWidth <= 660);
   };
 
   useEffect(() => {
+    checkMobile();
+
     window.addEventListener("resize", checkMobile);
 
-    return window.removeEventListener("resize", checkMobile);
+    return () => {
+      window.removeEventListener("resize", checkMobile);
+    };
   }, []);
 
+  const sectionClassName = `${styles.insightCardSection} ${
+    isMobile ? styles.mobileSection : styles.desktopSection
+  }`;
+
   return (
-    <div className={styles.insightCardSection}>
-      {posts.map((insight) => (
-        <div key={insight.post_id}>
-          <InsightItem insight={insight} />
+    <div className={sectionClassName} ref={isMobile ? emblaRef : undefined}>
+      {isMobile ? (
+        <div className={styles.insightTrack}>
+          {posts.map((insight) => (
+            <div className={styles.insightSlide} key={insight.post_id}>
+              <InsightItem insight={insight} />
+            </div>
+          ))}
         </div>
-      ))}
+      ) : (
+        posts.map((insight) => (
+          <div key={insight.post_id}>
+            <InsightItem insight={insight} />
+          </div>
+        ))
+      )}
     </div>
   );
 }
