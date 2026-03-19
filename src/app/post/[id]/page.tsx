16f53171f12340883c38239e350e96afea30930f
n_joid <72697791+nakisara01@users.noreply.github.com>
2026-03-19 15:38:36 +0900

    [#39] fix: trailing slash error

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -9,12 +9,25 @@ import { Suspense } from "react";
 export default async function PostDetail({
   params,
 }: {
-  params: { id: string };
+  params: { id: string }; //Next가 page를 streaming & async rendering으로 처리한다. page가 async server component일 수 있기 때문에 params를 Promise로 전달한다.
 }) {
   const { id } = params;
-  const response = await fetch(`${API_URLS.posts}${id}`, {
-    cache: "no-store",
-  });
 
-  return <div>status: {response.status}</div>;
+  const response = await fetch(`${API_URLS.posts}${id}/`);
+  if (!response.ok) {
+    throw new Error(`Failed to fetch post: ${response.status}`);
+  }
+  const post = await response.json();
+  console.log(post);
+
+  return (
+    <div className="postPage">
+      <TopPost post={post} />
+      <PostContent content={post.content} />
+      <DailyTogether />
+      <Suspense fallback={null}>
+        <AnotherInsight id={id} />
+      </Suspense>
+    </div>
+  );
 }
