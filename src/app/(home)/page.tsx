4cdbf06d468b126c19aa607b85ad514ab9422e5d
n_joid <72697791+nakisara01@users.noreply.github.com>
2026-03-20 01:33:42 +0900

    [#42] fix: page metadata

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,6 +4,11 @@ import UpButton from "../../components/upButton";
 import "./home.css";
 import PostList from "./components/post/postList";
 import { Suspense } from "react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "데일리펀딩의 특별한 블로그, 데일리 인사이트",
+};
 
 export default function Home() {
   return (
