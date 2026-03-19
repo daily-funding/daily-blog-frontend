12dcfa6bcfde0b2102f502fa1f35ec7bdb410560
n_joid <72697791+nakisara01@users.noreply.github.com>
2026-03-19 14:30:41 +0900

    [#30] fix: add sanitize process

--- src/app/post/[id]/components/postContent/index.tsx
+++ src/app/post/[id]/components/postContent/index.tsx
@@ -1,12 +1,14 @@
 import PostShareButton from "./components/postShareButton";
+import DOMPurify from "isomorphic-dompurify";
 import "./style.css";
 
 export default function PostContent({ content }: { content: string }) {
+  const sanitizedContent = DOMPurify.sanitize(content);
   return (
     <div className="postContent">
       <div
         className="blog-post-content"
-        dangerouslySetInnerHTML={{ __html: content }}
+        dangerouslySetInnerHTML={{ __html: sanitizedContent }}
       />
       <PostShareButton />
     </div>
